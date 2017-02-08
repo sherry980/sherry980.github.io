3d64e5334a4f13e57efee94d4098909ab56d3062
Sheharyar Khushnood <shaykhushnood@gmail.com>
2017-02-08 09:11:38 +0500

    hello

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -24,33 +24,7 @@ if (typeof jQuery === 'undefined') {
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
-$(function()
-  { $.getJSON("https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=ee316f45a2f64f938f9d40d6684c323c", 
-    function(data){ 
-    var art= data.articles 
-$(".news").append(art[0].title );
-$(".des").append(art[0].description );
-// $(".url").append(art[0].url); 
 
-$(".img1").attr('src', art[0].urlToImage);
-
-
-$(".news2").append(art[1].title );
-$(".des2").append(art[1].description );
-$("img2").append("" + art[1].urltoImage + ""); 
-$(".img2").attr('src', art[1].urlToImage);
-
-$(".news3").append(art[2].title );
-$(".des3").append(art[2].description );
-$("img3").append("" + art[2].urltoImage + ""); 
-$(".img3").attr('src', art[2].urlToImage);
-
-
- 
-      console.log(eart[2].title); 
-
-  });
-});
 
 
 +function ($) {
